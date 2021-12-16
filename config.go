e5397cdcd1e0b6954242d67ee05fe7a8aa66c321
xwzQmxx <1499273991@qq.com>
2021-12-16 18:04:16 +0800

    save step 1

--- config.go
+++ config.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/opensourceways/community-robot-lib/config"
+import (
+	"fmt"
+
+	"github.com/opensourceways/community-robot-lib/config"
+)
 
 type configuration struct {
 	ConfigItems []botConfig `json:"config_items,omitempty"`
@@ -13,6 +17,7 @@ func (c *configuration) configFor(org, repo string) *botConfig {
 
 	items := c.ConfigItems
 	v := make([]config.IRepoFilter, len(items))
+
 	for i := range items {
 		v[i] = &items[i]
 	}
@@ -20,6 +25,7 @@ func (c *configuration) configFor(org, repo string) *botConfig {
 	if i := config.Find(org, repo, v); i >= 0 {
 		return &items[i]
 	}
+
 	return nil
 }
 
@@ -34,6 +40,7 @@ func (c *configuration) Validate() error {
 			return err
 		}
 	}
+
 	return nil
 }
 
@@ -50,11 +57,62 @@ func (c *configuration) SetDefault() {
 
 type botConfig struct {
 	config.RepoFilter
+
+	// MenterPath the file  path of menter.json
+	MenterPath string `json:"menter_file_path" required:"true"`
+
+	// IssueCommetTplPath the file path of issue comment template
+	IssueCommetTplPath string `json:"issue_comment_tpl_path" required:"true"`
+
+	// DescisionTplPath the file path of descision template
+	DescisionTplPath string `json:"descision_tpl_path" required:"true"`
+
+	// PRCommentTplPath the file path of pull request comment template
+	PRCommentTplPath string `json:"pr_comment_tpl_path" required:"true"`
+
+	// ParitiTplPath  the file path of pariti template
+	ParitiTplPath string `json:"pariti_tpl_path" required:"true"`
+
+	// ParitiAITplPath the file path of pariti AI template
+	ParitiAITplPath string `json:"pariti_ai_tpl_path" required:"true"`
+
+	// AssignTplPath the file path of assign template
+	AssignTplPath string `json:"assign_tpl_path" required:"true"`
 }
 
 func (c *botConfig) setDefault() {
 }
 
 func (c *botConfig) validate() error {
+	cfgErr := "%s configuration items must be configured"
+
+	if c.MenterPath == "" {
+		return fmt.Errorf(cfgErr, "menter_file_path ")
+	}
+
+	if c.IssueCommetTplPath == "" {
+		return fmt.Errorf(cfgErr, "issue_comment_tpl_path")
+	}
+
+	if c.DescisionTplPath == "" {
+		return fmt.Errorf(cfgErr, "descision_tpl_path")
+	}
+
+	if c.PRCommentTplPath == "" {
+		return fmt.Errorf(cfgErr, "pr_comment_tpl_path")
+	}
+
+	if c.ParitiTplPath == "" {
+		return fmt.Errorf(cfgErr, "pariti_tpl_path")
+	}
+
+	if c.ParitiAITplPath == "" {
+		return fmt.Errorf(cfgErr, "pariti_ai_tpl_path")
+	}
+
+	if c.AssignTplPath == "" {
+		return fmt.Errorf(cfgErr, "assign_tpl_path")
+	}
+
 	return c.RepoFilter.Validate()
 }
